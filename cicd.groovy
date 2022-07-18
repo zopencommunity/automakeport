5976d1be72d3afe01a3effdca5313f4a64084ebe
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 08:46:49 -0400

    Add description to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux') 
 {
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'automakeport'), string(name: 'DESCRIPTION', 'automakeport' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'automakeport'), string(name: 'DESCRIPTION', 'GNU Automake is a tool for automatically generating Makefile.in files compliant with the GNU Coding Standards.' )]
         }
 }
