60959709ba5a5dfe08258c71bb269fb71dc3d3ac
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:11 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/automakeport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/automakeport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/automakeport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU Automake is a tool for automatically generating Makefile.in files compliant with the GNU Coding Standards.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/automakeport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU Automake is a tool for automatically generating Makefile.in files compliant with the GNU Coding Standards.' )]
         }
 }
